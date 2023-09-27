c529f05db16658020e3a090ce8c1e24cc56861b7
KatoIsa <katoisa1234@gmail.com>
2023-09-27 19:56:28 +0300

    Ui Update

--- js/main.js
+++ js/main.js
@@ -2,11 +2,9 @@ const App = {
     RandomImageGenrator: () =>{
         document.addEventListener("DOMContentLoaded", function() {
             const randomImages = [
-                "./assets/Icon-1.png",
-                "./assets/icon-2.jpg",
-                "./assets/icon-4.jpg",
-                "./assets/icon.jpg",
-                // Add more image URLs as needed
+                "./assets/Random-1.png",
+                "./assets/Random-3.png",
+                "./assets/Random-4.png",
             ];
 
             const randomImageElements = document.querySelectorAll(".card");
@@ -16,10 +14,33 @@ const App = {
                 const randomImageUrl = randomImages[randomIndex];
                 element.style.backgroundImage = `url(${randomImageUrl})`;
             });
-            console.log(randomImageElements)
         });
+
+    },
+    Topicnumber: ()=>{
+        // get all card elements and store there length
+        const cards = document.querySelectorAll('.card');
+        let cardNumber = cards.length;
+
+        // update the number of topics.
+        const topicNumber = document.querySelector('.topicNumber');
+        topicNumber.innerText = cardNumber;
+
+        // add numbering to the topics ....
+        let numberingDiv = document.createElement('div');
+        numberingDiv.classList.add('numberingDiv');
+        console.log(numberingDiv);
+
+        // for(i = 0; i <= cards.length - 1 ; i++){
+        //     cards[i].append(numberingDiv);
+        // }
+        // cards.forEach((a) => {
+        //     a.append(numberingDiv);
+        //     console.log(a)
+        // })
 
     }
 }
 
 App.RandomImageGenrator();
+App.Topicnumber();
